7700b64fff3a78949f7c6151cda8be0e10be89e3
pascalgrcic <pascalgrcic@macbook-pro-von-pascal.fritz.box>
2023-06-08 16:27:00 +0200

    Betreuer und Studenten Abgaben funktionieren

--- src/autobatch/gui/betreuer/BetreuerStudenten_1Panel.java
+++ src/autobatch/gui/betreuer/BetreuerStudenten_1Panel.java
@@ -1,13 +1,17 @@
 package autobatch.gui.betreuer;
 
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import autobatch.businessobjects.Betreuer;
 import autobatch.businessobjects.Student;
+import autobatch.businesslogic.actionlistener.AbgabeHinzufuegenActionListener;
 import autobatch.businesslogic.itemlistener.NdaItemListener;
+import autobatch.businesslogic.mouselistener.downloadFileMouseListener;
 import autobatch.businessobjects.Arbeit;
 import autobatch.dbaccess.Datenbankabfrage;
 import autobatch.navigation.PanelSwitcher;
@@ -17,12 +21,26 @@ import javax.swing.JLabel;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JCheckBox;
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.JScrollPane;
+import java.awt.Component;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class BetreuerStudenten_1Panel extends JPanel {
 
 	private PanelSwitcher panelSwitcher;
 	private Betreuer betreuer;
 	private final ButtonGroup buttonGroupNDA = new ButtonGroup();
+    private DefaultListModel<String> listModel;
+    private JList<String> fileList;
+    private DefaultListModel<String> listModel_1;
+    private JList<String> fileList_1;
+    private Datenbankabfrage dbaccess;
+	
 
 	public BetreuerStudenten_1Panel(PanelSwitcher panelSwitcher, Betreuer betreuer) {
 
@@ -30,13 +48,13 @@ public class BetreuerStudenten_1Panel extends JPanel {
 		setPreferredSize(new Dimension(1000, 500));
 		setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		Datenbankabfrage datenbankabfrage = new Datenbankabfrage();
+		Datenbankabfrage dbaccess = new Datenbankabfrage();
 
 		int mnr = Integer.parseInt(panelSwitcher.getData("2") + "");
-		Student student = datenbankabfrage.getStudentByMNR(mnr);
+		Student student = dbaccess.getStudentByMNR(mnr);
 
 		int idThema = Integer.parseInt(panelSwitcher.getData("3") + "");
-		Arbeit arbeit = datenbankabfrage.getArbeitByID(idThema);
+		Arbeit arbeit = dbaccess.getArbeitByID(idThema);
 
 		BetreuerNavigationBar betreuerNavigationBar = new BetreuerNavigationBar(panelSwitcher);
 
@@ -98,61 +116,206 @@ public class BetreuerStudenten_1Panel extends JPanel {
 		JLabel lblNewLabel_10 = new JLabel("New label");
 
 		JLabel lblNewLabel_11 = new JLabel("New label");
+		
+		JLabel lblBetreuerAbgaben = new JLabel("Bisherige Abgaben:");
+		
+		JLabel lblStudentAbgaben = new JLabel("Student:");
+		
+		JLabel lbl_success = new JLabel("Dokument erfolgreich hochgeladen!");
+		lbl_success.setForeground(new Color(61, 255, 30));
+		lbl_success.setVisible(false);
+		
+		JLabel lbl_error = new JLabel("Fehler beim Hochladen");
+		lbl_error.setForeground(new Color(251, 44, 22));
+		lbl_error.setVisible(false);
+		
+		listModel = new DefaultListModel<>();
+        ArrayList<String> submissions = (ArrayList<String>) dbaccess.getSubmissions(betreuer.getBenutzername());
+        for(String submission : submissions) {
+            listModel.addElement(submission);
+        }
+        
+        listModel_1 = new DefaultListModel<>();
+        ArrayList<String> submissions_1 = (ArrayList<String>) dbaccess.getSubmissions(student.getBenutzername());
+        System.out.println(student.getBenutzername());
+        for(String submission : submissions_1) {
+            listModel_1.addElement(submission);
+        }
+		
+		fileList = new JList<>(listModel);
+        fileList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        fileList_1 = new JList<>(listModel_1);
+        fileList_1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		JScrollPane fileListScrollPane = new JScrollPane(fileList);
+		
+		JScrollPane fileListScrollPane_1 = new JScrollPane(fileList_1);
+		
+		fileList_1.addMouseListener(new downloadFileMouseListener() {
+            public void mouseClicked(MouseEvent evt) {
+                JList list = (JList)evt.getSource();
+                if (evt.getClickCount() == 2) { // Double-click
+                    int index = list.locationToIndex(evt.getPoint());
+                    String filename = listModel.getElementAt(index);
+                    downloadFile(filename);
+                }
+            }
+        });
+		
+		fileList.addMouseListener(new downloadFileMouseListener() {
+            public void mouseClicked(MouseEvent evt) {
+                JList list = (JList)evt.getSource();
+                if (evt.getClickCount() == 2) { // Double-click
+                    int index = list.locationToIndex(evt.getPoint());
+                    String filename = listModel.getElementAt(index);
+                    downloadFile(filename);
+                }
+            }
+        });
+		
+		JButton btn_uploadDocument = new JButton("Dokument Hochladen");
+		btn_uploadDocument.addActionListener(new AbgabeHinzufuegenActionListener(lbl_success, lbl_error));
+		
+		
+		
+		// Hier den upload der PDF Dateien für den Betreuer ermöglichen
+		
+		
+		
+		// Hier das einsehen der Abagben des Studenten ermöglichen
+		
 
 		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addComponent(betreuerNavigationBar, GroupLayout.DEFAULT_SIZE, 990, Short.MAX_VALUE)
-				.addGroup(groupLayout.createSequentialGroup().addContainerGap().addComponent(lblStarttermin)
-						.addContainerGap(910, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup().addContainerGap().addComponent(lblEmail)
-						.addContainerGap(946, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup().addContainerGap()
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING).addComponent(lblName)
-								.addComponent(lblThema).addComponent(lblIPAnfang).addComponent(lblNewLabel_5))
-						.addGap(74)
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING).addComponent(lblNewLabel_9)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblEmail)
+					.addContainerGap(946, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblName)
+						.addComponent(lblThema)
+						.addComponent(lblIPAnfang)
+						.addComponent(lblNewLabel_5))
+					.addGap(74)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel_9)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel_8)
+								.addComponent(lblNameIn)
+								.addComponent(lblThemaIN)
+								.addComponent(lblNewLabel_6)
+								.addComponent(lblNewLabel_7))
+							.addGap(227)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel)
+								.addComponent(lblNewLabel_1)
+								.addComponent(lblNewLabel_2)
+								.addComponent(lblNewLabel_3)
+								.addComponent(lblNewLabel_4))
+							.addGap(79)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel_11)
+								.addComponent(lblNewLabel_10)
 								.addGroup(groupLayout.createSequentialGroup()
-										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-												.addComponent(lblNewLabel_8).addComponent(lblNameIn)
-												.addComponent(lblThemaIN).addComponent(lblNewLabel_6)
-												.addComponent(lblNewLabel_7))
-										.addGap(227)
-										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-												.addComponent(lblNewLabel).addComponent(lblNewLabel_1)
-												.addComponent(lblNewLabel_2).addComponent(lblNewLabel_3)
-												.addComponent(lblNewLabel_4))
-										.addGap(79)
-										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-												.addComponent(lblNewLabel_11).addComponent(lblNewLabel_10)
-												.addGroup(groupLayout.createSequentialGroup().addComponent(chckbxNDAJa)
-														.addPreferredGap(ComponentPlacement.RELATED)
-														.addComponent(chckbxNDANein))
-												.addComponent(lblAbgabeterminIN).addComponent(lblmnrIN))))
-						.addContainerGap(258, Short.MAX_VALUE)));
-		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
-				.createSequentialGroup()
-				.addComponent(betreuerNavigationBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-						GroupLayout.PREFERRED_SIZE)
-				.addGap(18)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(lblName)
-						.addComponent(lblNameIn).addComponent(lblNewLabel).addComponent(lblmnrIN))
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(lblStarttermin)
-						.addComponent(lblNewLabel_1).addComponent(lblAbgabeterminIN).addComponent(lblNewLabel_6))
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addGroup(groupLayout
-						.createParallelGroup(Alignment.BASELINE).addComponent(lblEmail).addComponent(lblNewLabel_2)
-						.addComponent(chckbxNDAJa).addComponent(chckbxNDANein).addComponent(lblNewLabel_7))
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(lblThema)
-						.addComponent(lblThemaIN).addComponent(lblNewLabel_3).addComponent(lblNewLabel_10))
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(lblIPAnfang)
-						.addComponent(lblNewLabel_4).addComponent(lblNewLabel_8).addComponent(lblNewLabel_11))
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(lblNewLabel_5)
-						.addComponent(lblNewLabel_9))
-				.addContainerGap(296, Short.MAX_VALUE)));
+									.addComponent(chckbxNDAJa)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(chckbxNDANein))
+								.addComponent(lblAbgabeterminIN)
+								.addComponent(lblmnrIN))))
+					.addContainerGap(258, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(46)
+							.addComponent(lblStarttermin))
+						.addComponent(lblStudentAbgaben, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(864, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(fileListScrollPane, GroupLayout.PREFERRED_SIZE, 260, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lbl_success)
+						.addComponent(lbl_error))
+					.addContainerGap(480, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(fileListScrollPane_1, GroupLayout.PREFERRED_SIZE, 260, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(724, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblBetreuerAbgaben)
+					.addContainerGap(864, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btn_uploadDocument)
+					.addContainerGap(867, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(betreuerNavigationBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblName)
+						.addComponent(lblNameIn)
+						.addComponent(lblNewLabel)
+						.addComponent(lblmnrIN))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblStarttermin)
+						.addComponent(lblNewLabel_1)
+						.addComponent(lblAbgabeterminIN)
+						.addComponent(lblNewLabel_6))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblEmail)
+						.addComponent(lblNewLabel_2)
+						.addComponent(chckbxNDAJa)
+						.addComponent(chckbxNDANein)
+						.addComponent(lblNewLabel_7))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblThema)
+						.addComponent(lblThemaIN)
+						.addComponent(lblNewLabel_3)
+						.addComponent(lblNewLabel_10))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblIPAnfang)
+						.addComponent(lblNewLabel_4)
+						.addComponent(lblNewLabel_8)
+						.addComponent(lblNewLabel_11))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblNewLabel_5)
+								.addComponent(lblNewLabel_9))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblBetreuerAbgaben)
+							.addGap(5)
+							.addComponent(fileListScrollPane, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
+							.addGap(9)
+							.addComponent(btn_uploadDocument)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblStudentAbgaben)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(fileListScrollPane_1, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(65)
+							.addComponent(lbl_success)
+							.addGap(18)
+							.addComponent(lbl_error)))
+					.addContainerGap(22, Short.MAX_VALUE))
+		);
 		setLayout(groupLayout);
 
 	}
--- src/autobatch/gui/student/StudentNavigationBar.java
+++ src/autobatch/gui/student/StudentNavigationBar.java
@@ -92,21 +92,36 @@ public class StudentNavigationBar extends JPanel {
 		});
 
 		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup().addGap(63)
-						.addComponent(lbl_Ip, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE).addGap(54)
-						.addComponent(lbl_betreuer, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
-						.addGap(37)
-						.addComponent(lbl_abgaben, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE)
-						.addGap(47).addComponent(lbl_formulare).addGap(64).addComponent(lbl_daten)
-						.addPreferredGap(ComponentPlacement.RELATED, 360, Short.MAX_VALUE).addComponent(lbl_abmelden)
-						.addGap(32)));
-		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-				.addGroup(groupLayout.createSequentialGroup().addContainerGap(19, Short.MAX_VALUE)
-						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE).addComponent(lbl_Ip)
-								.addComponent(lbl_betreuer).addComponent(lbl_formulare).addComponent(lbl_abgaben)
-								.addComponent(lbl_daten).addComponent(lbl_abmelden))
-						.addGap(15)));
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(42)
+					.addComponent(lbl_betreuer, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(lbl_Ip, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+					.addGap(28)
+					.addComponent(lbl_formulare)
+					.addGap(42)
+					.addComponent(lbl_abgaben, GroupLayout.PREFERRED_SIZE, 68, GroupLayout.PREFERRED_SIZE)
+					.addGap(38)
+					.addComponent(lbl_daten)
+					.addPreferredGap(ComponentPlacement.RELATED, 457, Short.MAX_VALUE)
+					.addComponent(lbl_abmelden)
+					.addGap(32))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(19, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbl_abmelden)
+						.addComponent(lbl_formulare)
+						.addComponent(lbl_abgaben)
+						.addComponent(lbl_betreuer)
+						.addComponent(lbl_Ip)
+						.addComponent(lbl_daten))
+					.addGap(15))
+		);
 		setLayout(groupLayout);
 
 	}
